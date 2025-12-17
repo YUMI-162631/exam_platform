5a28e4914e0264c40e19638afc513bb8084a73ed
YUMI-162631 <hasumi.ym162631@gmail.com>
2025-12-17 11:48:43 +0900

    Fix DATABASES initialization

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,14 @@ else:
         }
     }
 """
+# 初期のデータベース設定(SQLite)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
